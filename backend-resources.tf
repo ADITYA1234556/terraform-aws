2d8471ac17675ea230e9ce1c0d0b4b16cb3e2904
ADITYA1234556 <adityanavaneethan98@gmail.com>
2024-09-21 09:33:17 +0100

    fixed rds

--- backend-resources.tf
+++ backend-resources.tf
@@ -15,15 +15,15 @@ resource "aws_elasticache_subnet_group" "aditya-elasticache-subgrp" {
 }
 
 resource "aws_db_instance" "aditya-rds" {
-  instance_class         = "db.t2.micro"
+  instance_class         = "db.t3.micro"
   allocated_storage      = 20
   storage_type           = "gp2"
   engine                 = "mysql"
-  engine_version         = "5.6.34"
+  engine_version         = "8.0.35"
   db_name                = var.DBNAME
   username               = var.DBUSER
   password               = var.DBPASS
-  parameter_group_name   = "default.mysql5.6"
+  parameter_group_name   = "default.mysql8.0"
   multi_az               = "false"
   publicly_accessible    = "false"
   skip_final_snapshot    = true
@@ -36,7 +36,7 @@ resource "aws_elasticache_cluster" "aditya-cache" {
   engine               = "memcached"
   node_type            = "cache.t2.micro"
   num_cache_nodes      = 1
-  parameter_group_name = "default.memcached1.5"
+  parameter_group_name = "default.memcached1.6"
   port                 = 11211
   security_group_ids   = [aws_security_group.aditya-backend-sg.id]
   subnet_group_name    = aws_elasticache_subnet_group.aditya-elasticache-subgrp.name
@@ -45,7 +45,7 @@ resource "aws_elasticache_cluster" "aditya-cache" {
 resource "aws_mq_broker" "aditya-mq" {
   broker_name        = "aditya-mq"
   engine_type        = "ActiveMQ"
-  engine_version     = "5.15.0"
+  engine_version     = "5.16.7"
   host_instance_type = "mq.t2.micro"
   security_groups    = [aws_security_group.aditya-backend-sg.id]
   subnet_ids         = [module.vpc.private_subnets[0]]
